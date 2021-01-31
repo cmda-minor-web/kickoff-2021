e97d2cf7cf578a9f3ea21380ee73c86ebd56ba2d
Joost Faber <j.faber@hva.nl>
2021-01-31 21:04:29 +0100

    added GET & PUT requests

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,7 +1,38 @@
-// Get teams from API
-const url = "https://600ff44f6c21e1001704fac2.mockapi.io/minor-web/api/teams"
+const url = 'https://600ff44f6c21e1001704fac2.mockapi.io/minor-web/api/'
 
-const teams = fetch(url)
+// GET REQUEST
+const teams = fetch(`${url}/squads/1/teams/3/members/`)
                 .then(response => response.json())
-                .then(data => console.log(data));
+                .then(data => console.log('fetch', data));
 
+// PUT REQUEST
+const putData = {
+    id:1,
+    teamId:1,
+    name:'Sanne',
+    prefix:'',
+    surname:'Duinker',
+    mugshot:'',
+    githubHandle:'',
+    other:{
+        hobbies: 'fishing',
+        color: 'aqua',
+        skills: ['vue', 'react']
+    }
+}
+
+async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'PUT',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return response.json();
+}
+  
+postData(`${url}/squads/1/teams/1/members/1`, putData)
+    .then(data => {
+        console.log('put', data);
+    });
