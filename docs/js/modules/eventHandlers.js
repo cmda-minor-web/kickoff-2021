267afa5da07b1bb8839fb64d06c5b115831f07af
Joost Faber <j.faber@hva.nl>
2021-02-08 20:25:50 +0100

    added todo comment

--- docs/js/modules/eventHandlers.js
+++ docs/js/modules/eventHandlers.js
@@ -3,6 +3,6 @@ const clearStorage = document.querySelector('footer button');
 export function refetchAPI(){
     clearStorage.addEventListener('click', () => {
         localStorage.removeItem('tribe');
-        window.location.reload(false); 
+        window.location.reload() // ugly hack, replace with routing
     })
 }
